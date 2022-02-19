5e5a3ec67824e6e1d8bdbefbea5f1041400f3d64
kangCoder <khsofficial1213@gmail.com>
2022-02-19 22:07:03 +0900

    2447 별찍기

---
+++ baekjoon_17413.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <stack>
+using namespace std;
+
+int main() {
+    string S;
+    getline(cin, S);
+    
+    stack<char> st;
+    for (int i = 0; i < S.size(); i++) {
+        if (S[i] == '<') {
+            
+        }
+    }
+    return 0;
+}
---
+++ baekjoon_2447.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+void star(int N, int i, int j) {
+    if ((i / N) % 3 == 1 && (j / N) % 3 == 1) {
+        cout << " ";
+    } else {
+        if (N / 3 == 0) {
+            cout << "*";
+        } else {
+            star(N / 3, i, j);
+        }
+    }
+}
+
+int main() {
+    int N;
+    cin >> N;
+    
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            star(N, i, j);
+        }
+        cout << "\n";
+    }
+    return 0;
+}
