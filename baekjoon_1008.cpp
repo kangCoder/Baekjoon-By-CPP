50d3faee44b056387c3e1bfdd234b0e2c103de03
kangCoder <khsofficial1213@gmail.com>
2022-02-19 22:01:03 +0900

    1008 기본

---
+++ baekjoon_1008.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int A, B;
+    cin >> A >> B;
+    double C = (double)A/B;
+    cout.precision(10);
+    cout << C << endl;
+    return 0;
+}
